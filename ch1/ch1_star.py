7fb6e995d7502df2ba3af81cc92b79ec8726acce
psh000124 <80241873+psh000124@users.noreply.github.com>
2021-03-15 17:54:57 +0900

    Add files via upload

--- ch1/ch1_star.py
+++ ch1/ch1_star.py
@@ -1,17 +1,35 @@
-#numeric data
-a=2
-b=3
-a+b
-# string
-s1 = "Hello World!"
-print(s1)
-# data type : list
-x = [1,2,3]
-y = [4,5,6]
-x+y
-# function
-def add(a,b): 
-    return a+b
+# ch2_01_numeric.py
+#
+print("숫자형: 정수")
+a = 123
+a = -178
+a = 0
+print("숫자형: 실수")
+a = 1.2
+a = -3.48
+a = 4.24e10
+a = 4.24e-10
+print("숫자형: 8진수와 16진수")
+a = 0o177
+a
+a = 0x8FF
+a
+a = 0xABC
+a
+print("숫자형: 연산자와 연산")
+a = 3
+b = 4
+a + b
+a * b
+a / b
+a ** b
+a % b
+a // b
 
-add(1,2)
-add(x,y)
+14 // 3
+14 % 3
+
+"""
+Author: redwoods
+파이썬 코드: ch2_01_numeric.py
+"""
